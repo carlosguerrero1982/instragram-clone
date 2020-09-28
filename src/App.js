27ec145c7391343078f138b73d6c764d54948af1
= <=>
2020-09-28 18:05:16 +0200

    new today

--- src/App.js
+++ src/App.js
@@ -248,26 +248,38 @@ function App() {
     
     <div className="app_posts">
 
+      <div className="app_postLeft">
+
       {posts.map(({id,post}) =>(
 
-        <Post key={id} username= {post.username} caption = {post.caption} imageUrl={post.imageUrl} />
+        <Post key={id} postId={id} username= {post.username} caption = {post.caption} imageUrl={post.imageUrl} />
 
       ))}
 
+      </div>
+  
+  <div className="app_postRight"> 
+
+      <InstagramEmbed
+      url='https://instagr.am/p/Zw9o4/'
+      maxWidth={320}
+      hideCaption={false}
+      containerTagName='div'
+      protocol=''
+      injectScript
+      onLoading={() => {}}
+      onSuccess={() => {}}
+      onAfterRender={() => {}}
+      onFailure={() => {}}
+    />
+
+  </div>
+
+
+
 </div>
 
-<InstagramEmbed
-  url='https://instagr.am/p/Zw9o4/'
-  maxWidth={320}
-  hideCaption={false}
-  containerTagName='div'
-  protocol=''
-  injectScript
-  onLoading={() => {}}
-  onSuccess={() => {}}
-  onAfterRender={() => {}}
-  onFailure={() => {}}
-/>
+
 
 
 {user?.displayName ? (
--- src/Post.js
+++ src/Post.js
@@ -1,9 +1,43 @@
 import { Avatar } from '@material-ui/core';
-import React from 'react'
+import React, { useState,useEffect } from 'react'
 import './Post.css';
+import {db} from './firebase';
+
+function Post({username, caption, imageUrl, postId}) {
+
+    const[comments,setComments] = useState([]);
+    const[comment,setComment] = useState('');
 
 
-function Post({username, caption, imageUrl}) {
+    useEffect(() => {
+      
+        let unsubscribe;
+        if(postId){
+            unsubscribe=db
+                .collection("posts")
+                .doc(postId)
+                .collection("comments")
+                .onSnapshot((snapshot)=> {
+                    
+                    setComments(snapshot.docs.map((doc)=> doc.data()));
+
+                });
+
+        }
+
+
+    
+        return () => {
+            unsubscribe();
+        };
+    }, [postId])
+
+
+    const postComment = (event) =>{
+
+
+    }
+
     return (
         <div className="post">
 
@@ -25,6 +59,51 @@ function Post({username, caption, imageUrl}) {
 
             <h4 className="post_text"><strong>{username} </strong>{caption}</h4>
 
+            <div className="post_comments">
+
+            {
+                comments.map((comment)=>(
+
+                <p> 
+
+                     <strong>{comment.username}</strong> {comment.text}
+
+
+                </p>
+
+
+                )
+
+             )      
+
+            }
+            </div>
+
+            <form className="post_commentBox">
+
+                <input
+
+                    className="post_input"
+                    type="text"
+                    placeholder="Enter a comment"
+                    value={comment}
+                    onChange={(e)=> setComment(e.target.value)}
+
+                    />
+
+                    <button
+
+                        className="post_button"
+                        disabled={!comment} 
+                        type="submit"
+                        onClick={postComment}
+                        
+                    >
+
+                        POST
+
+                    </button>    
+            </form>
 
         </div>
     )
